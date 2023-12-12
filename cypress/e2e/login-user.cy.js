f71b610a044605971d788fd8d20bfe3adcbb1c96
Carlos Eduardo <cpwaldow@gmail.com>
2023-12-12 12:02:38 -0300

    test: criando cenário de login com conta sem e com dinheiro

--- cypress/e2e/login-user.cy.js
+++ cypress/e2e/login-user.cy.js
@@ -4,6 +4,13 @@ describe('Cenários de testes para Login de user no BugBank', () => {
     const userEmail = 'testeLogin@bugbank.com';
     const userName = 'Pericles da Silva';
     const userPassword = 'exaltaSambaEraBomDemais123#$';
+  });
+
+  it('Fazer login com sucesso - conta sem saldo', () => {
+    const userEmail = 'testeLogin@bugbank.com';
+    const userPassword = 'exaltaSambaEraBomDemais123#$';
+    const userName = 'Pericles da Silva';
+
     cy.get('button').contains('Registrar').click();
     cy.wait(100);
 
@@ -18,18 +25,42 @@ describe('Cenários de testes para Login de user no BugBank', () => {
     cy.get('button[type="submit"]:not(:first)').click({ force: true });
 
     cy.get('#btnCloseModal').click();
-  });
-
-  it('Fazer login com sucesso', () => {
-    const userEmail = 'testeLogin@bugbank.com';
-    const userPassword = 'exaltaSambaEraBomDemais123#$';
-    const userName = 'Pericles da Silva';
 
     cy.get('input[type="email"]').first().focus().type(userEmail);
     cy.get('input[type="password"]').first().focus().type(userPassword);
     cy.contains('Acessar').first().click();
     cy.get('#textName').should('have.text', `Olá ${userName},`);
     cy.contains('bem vindo ao BugBank :)').should('be.visible');
+    cy.contains('Saldo em conta R$ 0,00').should('be.visible');
+    cy.contains('Sair').click();
+  });
+
+  it('Fazer login com sucesso - conta com saldo', () => {
+    const userEmail = 'testeLogin@bugbank.com';
+    const userPassword = 'exaltaSambaEraBomDemais123#$';
+    const userName = 'Pericles da Silva';
+
+    cy.get('button').contains('Registrar').click();
+    cy.wait(100);
+
+    cy.get(':nth-child(2) > .input__default').focus().type(userEmail);
+
+    cy.get(':nth-child(3) > .input__default').focus().type(userName);
+
+    cy.get('input[type="password"]:not(:first)').each(($input) => {
+      cy.get($input).focus().type(userPassword);
+    });
+    cy.get('#toggleAddBalance').click({ force: true });
+    cy.get('button[type="submit"]:not(:first)').click({ force: true });
+
+    cy.get('#btnCloseModal').click();
+
+    cy.get('input[type="email"]').first().focus().type(userEmail);
+    cy.get('input[type="password"]').first().focus().type(userPassword);
+    cy.contains('Acessar').first().click();
+    cy.get('#textName').should('have.text', `Olá ${userName},`);
+    cy.contains('bem vindo ao BugBank :)').should('be.visible');
+    cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
     cy.contains('Sair').click();
   });
 
