e1ddbc228469de7b42096bda6306a7a2f193ae8f
Carlos Eduardo <cpwaldow@gmail.com>
2023-12-13 20:48:41 -0300

    test: Tentativa de transferência para conta inválida deve exibir mensagem de erro "Conta inválida ou inexistente"

--- cypress/e2e/transaction.cy.js
+++ cypress/e2e/transaction.cy.js
@@ -50,8 +50,6 @@ describe('Transição aprovadas entre contas', () => {
     cy.get('input[type="email"]').first().focus().type(userEmail);
     cy.get('input[type="password"]').first().focus().type(userPassword);
     cy.contains('Acessar').first().click();
-    cy.get('#textName').should('have.text', `Olá ${userName},`);
-    cy.contains('bem vindo ao BugBank :)').should('be.visible');
     cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
     cy.get('#btn-TRANSFERÊNCIA').click();
     cy.contains(
@@ -79,9 +77,60 @@ describe('Transição aprovadas entre contas', () => {
     cy.contains('Transferencia realizada com sucesso').should('be.visible');
     cy.contains('Fechar').click();
     cy.contains('Voltar').click();
-    cy.contains('Saldo em conta R$ 500,00');
+    cy.contains('Saldo em conta R$ 500,00').should('be.visible');
     cy.contains('Sair').click();
   });
+});
 
-  it('Tentativa de transferência para conta inválida deve exibir mensagem de erro "Conta inválida ou inexistente"', () => {});
+describe('Transação negada entre contas', () => {
+  beforeEach(() => {
+    cy.visit('https://bugbank.netlify.app/');
+  });
+  it('Tentativa de transferência para conta inválida deve exibir mensagem de erro "Conta inválida ou inexistente"', () => {
+    const userEmail = 'testeFalhaTransacao@bugbank.com';
+    const userPassword = 'senhaFalhaTransacao';
+    const userName = 'Martha Nunes';
+
+    cy.contains('Registrar').click();
+
+    cy.get('input').each(($input) => {
+      cy.get($input).focus().clear();
+    });
+
+    cy.get(':nth-child(2) > .input__default').focus().type(userEmail);
+
+    cy.get(':nth-child(3) > .input__default').focus().type(userName);
+
+    cy.get('input[type="password"]:not(:first)').each(($input) => {
+      cy.get($input).focus().type(userPassword);
+    });
+
+    cy.get('#toggleAddBalance').click({ force: true });
+
+    cy.get('button[type="submit"]:not(:first)').click({ force: true });
+
+    cy.get('#btnCloseModal').click();
+
+    cy.get('input[type="email"]').first().focus().type(userEmail);
+    cy.get('input[type="password"]').first().focus().type(userPassword);
+    cy.contains('Acessar').first().click();
+    cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
+    cy.get('#btn-TRANSFERÊNCIA').click();
+    cy.contains(
+      'Realize transferência de valores entre contas BugBank com taxa 0 e em poucos segundos.',
+    ).should('be.visible');
+
+    cy.get(':nth-child(1) > .input__default').focus().type(123);
+    cy.get('.account__data > :nth-child(2) > .input__default').focus().type(4);
+    cy.get(
+      '.styles__ContainerFormTransfer-sc-1oow0wh-0 > :nth-child(2) > .input__default',
+    )
+      .focus()
+      .type(500);
+    cy.contains('Transferir agora').click();
+    cy.contains('Conta inválida ou inexistente').should('be.visible');
+    cy.contains('Fechar').click();
+    cy.contains('Voltar').click();
+    cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
+  });
 });
