195f9ad0b39aab2623de6dbda45d5d7bd2c77a5d
Carlos Eduardo <cpwaldow@gmail.com>
2023-12-13 21:59:49 -0300

    test: Fazer login com sucesso - transação maior que o valor disponível

--- cypress/e2e/transaction.cy.js
+++ cypress/e2e/transaction.cy.js
@@ -80,9 +80,68 @@ describe('Transição aprovadas entre contas', () => {
     cy.contains('Saldo em conta R$ 500,00').should('be.visible');
     cy.contains('Sair').click();
   });
+
+  it('Fazer login com sucesso - transação maior que o valor disponível', () => {
+    const userEmail = 'maisDinheiro@bugbank.com';
+    const userPassword = 'maisDinheiro';
+    const userName = 'Belo';
+
+    cy.contains('Registrar').click();
+
+    cy.get('input').each(($input) => {
+      cy.get($input).focus().clear();
+    });
+
+    cy.get(':nth-child(2) > .input__default').focus().type(userEmail);
+
+    cy.get(':nth-child(3) > .input__default').focus().type(userName);
+
+    cy.get('input[type="password"]:not(:first)').each(($input) => {
+      cy.get($input).focus().type(userPassword);
+    });
+
+    cy.get('#toggleAddBalance').click({ force: true });
+
+    cy.get('button[type="submit"]:not(:first)').click({ force: true });
+
+    cy.get('#btnCloseModal').click();
+
+    cy.get('input[type="email"]').first().focus().type(userEmail);
+    cy.get('input[type="password"]').first().focus().type(userPassword);
+    cy.contains('Acessar').first().click();
+    cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
+    cy.get('#btn-TRANSFERÊNCIA').click();
+    cy.contains(
+      'Realize transferência de valores entre contas BugBank com taxa 0 e em poucos segundos.',
+    ).should('be.visible');
+
+    cy.getAllLocalStorage().then((result) => {
+      const assistantAccount =
+        result['https://bugbank.netlify.app']['contaAuxiliar@bugbank.com'];
+      const accountDigit =
+        JSON.parse(assistantAccount).accountNumber.split('-');
+      cy.get(':nth-child(1) > .input__default').focus().type(accountDigit[0]);
+      cy.get('.account__data > :nth-child(2) > .input__default')
+        .focus()
+        .type(accountDigit[1]);
+    });
+
+    cy.get(
+      '.styles__ContainerFormTransfer-sc-1oow0wh-0 > :nth-child(2) > .input__default',
+    )
+      .focus()
+      .type(1001);
+    cy.contains('Transferir agora').click();
+    cy.contains('Você não tem saldo suficiente para essa transação').should(
+      'be.visible',
+    );
+    cy.contains('Fechar').click();
+    cy.contains('Voltar').click();
+    cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
+  });
 });
 
-describe('Transação negada entre contas', () => {
+describe.skip('Transação negada entre contas', () => {
   beforeEach(() => {
     cy.visit('https://bugbank.netlify.app/');
   });
