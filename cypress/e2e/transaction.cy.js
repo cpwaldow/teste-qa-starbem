d8f38bb8bc7811a60a07e51398efb5afc7e6c7d5
Carlos Eduardo <cpwaldow@gmail.com>
2023-12-13 21:23:37 -0300

    test: Valor de transferência não pode ser igual ou menor que zero

--- cypress/e2e/transaction.cy.js
+++ cypress/e2e/transaction.cy.js
@@ -183,4 +183,108 @@ describe('Transação negada entre contas', () => {
     cy.contains('Voltar').click();
     cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
   });
+
+  it('Valor de transferência não pode ser igual a zero', () => {
+    const userEmail = 'transferenciaInvalida@bugbank.com';
+    const userPassword = 'transferenciaInvalida';
+    const userName = 'Lorena Magalhães';
+
+    cy.contains('Registrar').click();
+
+    cy.get('input').each(($input) => {
+      cy.get($input).focus().clear();
+    });
+
+    cy.get(':nth-child(2) > .input__default').focus().type(userEmail);
+
+    cy.get(':nth-child(3) > .input__default').focus().type(userName);
+
+    cy.get('input[type="password"]:not(:first)').each(($input) => {
+      cy.get($input).focus().type(userPassword);
+    });
+
+    cy.get('#toggleAddBalance').click({ force: true });
+
+    cy.get('button[type="submit"]:not(:first)').click({ force: true });
+
+    cy.get('#btnCloseModal').click();
+
+    cy.get('input[type="email"]').first().focus().type(userEmail);
+    cy.get('input[type="password"]').first().focus().type(userPassword);
+    cy.contains('Acessar').first().click();
+    cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
+    cy.get('#btn-TRANSFERÊNCIA').click();
+    cy.contains(
+      'Realize transferência de valores entre contas BugBank com taxa 0 e em poucos segundos.',
+    ).should('be.visible');
+
+    cy.get(':nth-child(1) > .input__default').focus().type('aaaaa');
+    cy.get('.account__data > :nth-child(2) > .input__default')
+      .focus()
+      .type('b');
+    cy.get(
+      '.styles__ContainerFormTransfer-sc-1oow0wh-0 > :nth-child(2) > .input__default',
+    )
+      .focus()
+      .type(0);
+    cy.contains('Transferir agora').click();
+    cy.contains('Valor da transferência não pode ser 0 ou negativo').should(
+      'be.visible',
+    );
+    cy.contains('Fechar').click();
+    cy.contains('Voltar').click();
+    cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
+  });
+
+  it('Valor de transferência não pode ser menor que zero', () => {
+    const userEmail = 'transferenciaInvalida@bugbank.com';
+    const userPassword = 'transferenciaInvalida';
+    const userName = 'Flávio Autobots';
+
+    cy.contains('Registrar').click();
+
+    cy.get('input').each(($input) => {
+      cy.get($input).focus().clear();
+    });
+
+    cy.get(':nth-child(2) > .input__default').focus().type(userEmail);
+
+    cy.get(':nth-child(3) > .input__default').focus().type(userName);
+
+    cy.get('input[type="password"]:not(:first)').each(($input) => {
+      cy.get($input).focus().type(userPassword);
+    });
+
+    cy.get('#toggleAddBalance').click({ force: true });
+
+    cy.get('button[type="submit"]:not(:first)').click({ force: true });
+
+    cy.get('#btnCloseModal').click();
+
+    cy.get('input[type="email"]').first().focus().type(userEmail);
+    cy.get('input[type="password"]').first().focus().type(userPassword);
+    cy.contains('Acessar').first().click();
+    cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
+    cy.get('#btn-TRANSFERÊNCIA').click();
+    cy.contains(
+      'Realize transferência de valores entre contas BugBank com taxa 0 e em poucos segundos.',
+    ).should('be.visible');
+
+    cy.get(':nth-child(1) > .input__default').focus().type('aaaaa');
+    cy.get('.account__data > :nth-child(2) > .input__default')
+      .focus()
+      .type('b');
+    cy.get(
+      '.styles__ContainerFormTransfer-sc-1oow0wh-0 > :nth-child(2) > .input__default',
+    )
+      .focus()
+      .type(-10);
+    cy.contains('Transferir agora').click();
+    cy.contains('Valor da transferência não pode ser 0 ou negativo').should(
+      'be.visible',
+    );
+    cy.contains('Fechar').click();
+    cy.contains('Voltar').click();
+    cy.contains('Saldo em conta R$ 1.000,00').should('be.visible');
+  });
 });
